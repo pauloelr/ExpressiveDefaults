6a5a98028cd96b8db0d540268767db645bb20a70
Paulo Eduardo <pauloelr@gmail.com>
2016-03-15 11:50:03 -0300

    Expressive Application

---
+++ config/autoload/routes.global.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'dependencies' => [
+        'invokables' => [
+            Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\FastRouteRouter::class,
+            App\Action\PingAction::class => App\Action\PingAction::class,
+        ],
+        'factories' => [
+            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
+            App\Action\PicturePageAction::class => App\Action\PicturePageFactory::class,
+        ],
+    ],
+
+    'routes' => [
+        [
+            'name' => 'home',
+            'path' => '/',
+            'middleware' => App\Action\HomePageAction::class,
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'api.ping',
+            'path' => '/api/ping',
+            'middleware' => App\Action\PingAction::class,
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'pic.view',
+            'path' => '/pic/view',
+            'middleware' => App\Action\PicturePageAction::class,
+            'allowed_methods' => ['GET'],
+        ],
+    ],
+];
